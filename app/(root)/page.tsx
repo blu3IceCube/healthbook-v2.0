bc382a9a4bdc5f1fbc60ee70b5f399d19707b556
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2023-09-02 14:14:22 +0530

    adding auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,8 @@ import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div className="ml-auto flex items-center space-x-4">
-      <UserButton />
+    <div className="p-4">
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
