fc4e87f3215446a95a8fd3fc8d6d48c9d5f29358
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2023-09-12 23:12:53 +0530

    removing popup

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <ModalProvider />
+          {/* <ModalProvider /> */}
           {children}
         </body>
       </html>
