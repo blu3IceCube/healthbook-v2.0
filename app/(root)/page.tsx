928868b0994f7648c4c0edde45d62aaf2848441b
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2023-09-13 19:31:47 +0530

    refactoring

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -8,30 +8,24 @@ import { useStoreModal } from "@/hooks/use-store-modal";
 import { Upload } from "lucide-react";
 import { useEffect } from "react";
 import { redirect } from "next/navigation";
-import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Home() {
-  // const onOpen = useStoreModal((state) => state.onOpen);
-  // const isOpen = useStoreModal((state) => state.isOpen);
-
-  // useEffect(() => {
-  //   if (!isOpen) {
-  //     onOpen();
-  //   }
-  // }, [isOpen, onOpen]);
+  const router = useRouter();
 
   return (
     <>
       <div className="m-auto w-9/12">
         <div className="flex items-center justify-between mt-12 mb-2">
           <h1 className="text-xl font-medium text-gray-500">Your files</h1>
-          <Link
+          <Button
             className="flex items-center justify-between gap-4 text-xl font-medium text-gray-500"
-            href="/upload"
+            onClick={() => router.push("/upload")}
+            variant="ghost"
           >
             <Upload />
             Upload
-          </Link>
+          </Button>
         </div>
         <Separator className="bg-gray-400" />
       </div>
